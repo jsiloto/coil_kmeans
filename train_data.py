0ed8871c8a683857fa75fd18c1bc16882273642e
Juliano Siloto Assine <jsiloto@cpqd.com.br>
2017-08-19 15:27:12 -0300

    Add instructions and work script

--- train_data.py
+++ train_data.py
@@ -1,13 +1,14 @@
 ## Initialisation
 
-import pandas as pd
 import numpy as np
-import matplotlib
-import matplotlib.pyplot as plt
 from PIL import Image
-import matplotlib.cm as cmx
-from mpl_toolkits.mplot3d import Axes3D
 from sklearn.cluster import KMeans
+
+
+# Instructions:
+# 1- Download coil-100.zip from
+#    http://www.cs.columbia.edu/CAVE/databases/SLAM_coil-20_coil-100/coil-100/coil-100.zip
+# 2- Extract it under ./coil-100
 
 image = Image.open("coil-100/obj1__0.png")
 imgvec = np.asarray(image)
@@ -19,5 +20,26 @@ kmeans.fit(df)
 labels = kmeans.predict(df)
 centroids = kmeans.cluster_centers_
 print(centroids)
+
+
+# Work Script
+
+# Build the training dataset
+# 1 - Load all images
+# 2 - Choose a subsample of images
+# 3 - Build a giant numpy array with all pixels from all images
+# 4 - Subsample this array in some way resulting in our training dataset
+
+# Train on our dataset
+# 5 - Apply K-Means on the dataset giving our feature space
+# 6 - Load all images separately
+# 7 - Convert each image to the feature space
+
+# Rank images
+# Chose a single image in feature space(A)
+# Calculate the euclidean distance from every image in feature space from A
+# Sort all distances
+# Choose 10 smaller distances
+# Plot images
 
 
