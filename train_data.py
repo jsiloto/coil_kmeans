6072661305b9bdc1a7cfc22aa47272549e0edeae
Juliano Siloto Assine <jsiloto@cpqd.com.br>
2017-08-19 16:22:56 -0300

    Subsample database and run K-Means

--- train_data.py
+++ train_data.py
@@ -3,54 +3,48 @@
 import numpy as np
 import os
 from PIL import Image
+import tables
 from sklearn.cluster import KMeans
-
-
-# Instructions:
-# 1- Download coil-100.zip from
-#    http://www.cs.columbia.edu/CAVE/databases/SLAM_coil-20_coil-100/coil-100/coil-100.zip
-# 2- Extract it under ./coil-100
-
-# image = Image.open("coil-100/obj1__0.png")
-# imgvec = np.asarray(image)
-# df = imgvec.reshape(imgvec.shape[0]*imgvec.shape[1], imgvec.shape[2])
-#
-# kmeans = KMeans(n_clusters=10)
-# kmeans.fit(df)
-#
-# labels = kmeans.predict(df)
-# centroids = kmeans.cluster_centers_
-# print(centroids)
-
-
-# Work Script
 
 # Build the training dataset
 # 1 - Load all images
 
 list_of_filenames = [filename for filename in os.listdir('./coil-100') if filename.endswith(".png")]
 list_of_filenames.sort()
-print list_of_filenames
 
 # 2 - Choose a subsample of images
 # Choosing all images
-list_of_imgs = [Image.open('./coil-100/' + filename) for filename in list_of_filenames]
-
 
 # 3 - Build a giant numpy array with all pixels from all images
-list_of_imgvecs = [np.asarray(img) for img in list_of_imgs]
-list_of_imgvecs = [imgvec.reshape(imgvec.shape[0]*imgvec.shape[1], imgvec.shape[2]) for imgvec in list_of_imgvecs]
+list_of_imgvecs = []
+for filename in list_of_filenames:
+    img = Image.open('./coil-100/' + filename)
+    imgvec = np.asarray(img)
+    imgvec = imgvec.reshape(imgvec.shape[0] * imgvec.shape[1], imgvec.shape[2])
+    img.close()
+    list_of_imgvecs += [imgvec]
 
 database = np.concatenate(list_of_imgvecs)
-print "OK"
-print database.shape
+print "Database Loaded"
+print "Database size = {}".format(database.shape)
 
-
-# 3 - Build a giant numpy array with all pixels from all images
 # 4 - Subsample this array in some way resulting in our training dataset
+percent = 0.001
+size = database.shape[0]
+subsample = np.random.choice(size, int(size*percent))
+database = database[subsample, :]
+print "Database Subsampled"
+print "Database size = {}".format(database.shape)
 
 # Train on our dataset
 # 5 - Apply K-Means on the dataset giving our feature space
+kmeans = KMeans(n_clusters=10)
+kmeans.fit(database)
+centroids = kmeans.cluster_centers_
+print(centroids)
+
+
+
 # 6 - Load all images separately
 # 7 - Convert each image to the feature space
 
